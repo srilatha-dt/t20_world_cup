58f19f01776332ca65c4f4be8de7082494091e57
DT_srialtha <sri.athota@dispatchtrack.com>
2024-07-19 16:03:03 +0530

    validation for player and team added

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+    validates :name, presence: true , length: { minimum: 3, maximum: 15}
+    validates :description, presence: true, length: {minimum: 10, maximum: 100}
     belongs_to :team
     enum role: [:allrounder, :bowler, :batsmen] 
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
+    validates :name, presence: true , length: { minimum: 3, maximum: 15}
+    validates :description, presence: true, length: {minimum: 10, maximum: 100}
     has_many :players, class_name: "Player", foreign_key: "team_id"
 end
